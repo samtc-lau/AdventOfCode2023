0bca959d0d97390673df0331865d6c796653281f
Sam Lau <samtclau@gmail.com>
2023-12-06 23:38:28 -0500

    Day6 improved

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,5 +1,5 @@
 ﻿//using var streamReader = new StreamReader("input.txt");
-//var output = 1;
+//var output = 1L;
 //var times = streamReader.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse);
 //var distances = streamReader.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse);
 //IEnumerable<(int time, int distance)> records = times.Zip(distances, (t, d) => (t, d));
@@ -20,33 +20,18 @@ var output = GetWaysToWin(time, distance);
 
 Console.WriteLine($"Part Two Answer: {output}");
 
-long GetDistance(int totalTime, int holdTime)
+long GetWaysToWin(long totalTime, long distanceToBeat)
 {
-    return (totalTime - holdTime) * (long)holdTime;
+    // Solving the quadratic equation get the 2 possible hold time for the distanceToBeat
+    // Since, (totalTime - holdTime) * holdTime = distance
+    // Hence, holdTime ^ 2 - totalTime * holdTime + distance = 0 (a = 1, b = -totalTime, c = distance)
+    (var holdTimeX, var holdTimeY) = GetQuadraticSolutions(1, -totalTime, distanceToBeat);
+
+    // Time between the 2 possible hold time are ways to win (travel longer than distanceToBeat)
+    return (long)Math.Floor(holdTimeX) - (long)Math.Floor(holdTimeY);
 }
 
-int GetWaysToWin(int totalTime, long distanceToBeat)
+(double x, double y) GetQuadraticSolutions (long a, long b, long c)
 {
-    // Assume all holding are win and deduct from here
-    var waysToWin = totalTime + 1;
-
-    // holding n ms is the same as holding (total time - n) ms, so only loop for the first half cases
-    foreach(var i in Enumerable.Range(0, totalTime / 2 + 1))
-    {
-        if (GetDistance(totalTime, i) > distanceToBeat)
-        {
-            // win
-            break;
-        }
-
-        // lose: reduce 2 counting the n and (total time - n) case
-        waysToWin -= 2;
-    }
-
-    if (waysToWin < 0)
-    {
-        waysToWin = 0;
-    }
-
-    return waysToWin;
+    return ((-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a), (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a));
 }
